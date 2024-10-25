f4022b48c7b5b669f4eed5f157ec978d50be77cc
dstout-devops <dstout.devops@gmail.com>
2024-10-25 15:08:19 +0000

    initial config

---
+++ src/ansible-full/README.md
@@ -0,0 +1,12 @@
+
+# Ansible-full (via pipx) (ansible)
+
+Ansible is a suite of software tools that enables infrastructure as code.
+
+## Example DevContainer Usage
+
+```json
+"features": {
+    "ghcr.io/devcontainers-contrib/features/ansible:2": {}
+}
+```
---
+++ src/ansible-full/install.sh
@@ -0,0 +1,24 @@
+set -e
+
+. ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-contrib/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+# and if missing - will download a temporary copy that automatically get deleted at the end 
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.6"
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/pipx-package:1" \
+    --option package='ansible-core' --option injections='ansible' --option includedeps='true'
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/pipx-package:1" \
+    --option package='ansible-creator' --option includedeps='true'
+    
+echo 'Done!'
